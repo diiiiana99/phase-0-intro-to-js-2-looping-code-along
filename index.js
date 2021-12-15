b088f9a2044f68a5ec45d8064574bcf04b1a8619
diiiiana99 <93669061+diiiiana99@users.noreply.github.com>
2021-12-14 20:30:55 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+let customMessages=[];
+
+function writeCards(names,eventName){
+
+    for (let i=0;i<names.length;i++){
+        console.log(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+        customMessages.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+
+    }
+return customMessages;
+}
+
+
+writeCardsn (["Ada, "Brendan", "Ali"], "birthday");
+
+function countDown(x){
+    while (x >= 0) {
+        console.log(x);
+        x--
+    }
+}
